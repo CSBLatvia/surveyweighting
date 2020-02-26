5ab96391bce2b3f4238387a057995a2c40ca7450
Juris Breidaks <juris.breidaks@csb.gov.lv>
2020-02-26 13:05:31 +0200

    Update

--- surveyweighting/R/nonresponse_correction.R
+++ surveyweighting/R/nonresponse_correction.R
@@ -7,7 +7,8 @@
 #' @param frame (\code{character}) - frame indicator variable name.
 #' @param response_ind (\code{character}) - response indicator variable name,  that indicates which respondents are weighted in the weight calculation process.
 #' @param summary_w (\code{character}) summary weight indicator variable name, that indicates which respondents have a weight greater than zero. Weight will be used to calculate summaries.
-#' @param design_weight teksts (\code{character}) - design weight variable name.
+#' @param design_weight (\code{character}) - design weight variable name.
+#' @param pop_size (\code{character}) - population size in strata variable name.
 #' @param strata (\code{character}) - strata variable name;
 #' @param dataset data.table;
 #'
@@ -32,10 +33,11 @@ nonresponse_correction <- function(in_system = "in_isdavs",
                                      response_ind = "ievad",
                                      summary_w = "kops_sv",
                                      design_weight = "diz_sv",
+                                     pop_size = NULL,
                                      strata = "strata",
                                      dataset){
 
-  ..design_weight <- koef <- ievad <- ievad_sum <- koef_nodot <- NULL
+  ..pop_size <- ..design_weight <- koef <- ievad <- ievad_sum <- koef_nodot <- NULL
 
   skaits_stratas <- dataset[, lapply(.SD, sum, na.rm = TRUE), keyby = strata,
                             .SDcols = c(response_ind, summary_w, frame, in_system, sample)]
@@ -47,7 +49,11 @@ nonresponse_correction <- function(in_system = "in_isdavs",
 
   dataset[, koef := 0]
 
-  dataset[get(response_ind) == 1, koef := get(response_ind) * get(..design_weight) * get(paste0(sample, "_sum")) / get(paste0(response_ind, "_sum"))]
+  if (is.null(pop_size)) {
+    dataset[get(response_ind) == 1, koef := get(response_ind) * get(..design_weight) * get(paste0(sample, "_sum")) / get(paste0(response_ind, "_sum"))]
+  } else {
+    dataset[get(response_ind) == 1, koef := get(response_ind) * get(..pop_size) / get(paste0(response_ind, "_sum"))]
+  }
 
   dataset[, koef_nodot := get(summary_w) * koef]
 
